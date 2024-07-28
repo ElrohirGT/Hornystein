f7a4bdc497d95dbe49be1e3e131efb999f5af639
ElrohirGT <elrohirgt@gmail.com>
2024-07-28 17:18:45 -0600

    feat: Added player movement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use hornystein::color::Color;
 use hornystein::framebuffer::{self, Framebuffer};
-use minifb::{Key, Window, WindowOptions};
+use minifb::{Key, KeyRepeat, Window, WindowOptions};
 use nalgebra_glm::{vec2_to_vec3, Vec2};
+use std::collections::VecDeque;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -23,6 +24,16 @@ struct Player {
     pub orientation: f32,
 }
 
+enum Message {
+    Advance(nalgebra_glm::Vec2),
+    Backwards(nalgebra_glm::Vec2),
+    RotateClockwise(f32),
+    RotateCounterClockwise(f32),
+}
+
+const PLAYER_SPEED: f32 = 1.5;
+const PLAYER_ROTATION_SPEED: f32 = std::f32::consts::FRAC_PI_8 / 4.0;
+
 fn main() {
     let window_width = 800;
     let window_height = 800;
@@ -41,33 +52,66 @@ fn main() {
         Window::new("Hornystein", window_width, window_height, window_options).unwrap();
 
     let mut frame_count: usize = 0;
-    let frame_update_timer: usize = 6000;
+    let frame_update_timer: usize = 1;
     framebuffer.set_background_color(0x00ff00);
     framebuffer.set_current_color(0xc35817);
 
     let mut data = init(framebuffer_width, framebuffer_height);
     render(&mut framebuffer, &data);
 
+    let mut message_queue = VecDeque::with_capacity(100);
     while window.is_open() {
         // listen to inputs
         if window.is_key_down(Key::Escape) {
             break;
         }
 
-        // Update the model
-        if frame_count == frame_update_timer {
-            frame_count = 0;
-            data = update(data);
-            // Render
-            render(&mut framebuffer, &data);
+        // Update and render the model
+        if frame_count != frame_update_timer {
+            frame_count += 1;
+            // Update the window with the framebuffer contents
+            window
+                .update_with_buffer(&framebuffer.buffer, framebuffer_width, framebuffer_height)
+                .expect("Couldn't update the framebuffer!");
+            continue;
         }
 
-        // Update the window with the framebuffer contents
+        frame_count = 0;
         window
-            .update_with_buffer(&framebuffer.buffer, framebuffer_width, framebuffer_height)
-            .expect("Couldn't update the framebuffer!");
+            .get_keys_pressed(KeyRepeat::Yes)
+            .iter()
+            .for_each(|key| match (key, message_queue.front()) {
+                (Key::W, Some(Message::Advance(_))) => {}
+                (Key::W, _) => {
+                    let x_delta = PLAYER_SPEED * data.player.orientation.cos();
+                    let y_delta = PLAYER_SPEED * data.player.orientation.sin();
+                    message_queue
+                        .push_back(Message::Advance(nalgebra_glm::Vec2::new(x_delta, y_delta)));
+                }
+                (Key::S, Some(Message::Backwards(_))) => {}
+                (Key::S, _) => {
+                    let x_delta = PLAYER_SPEED * data.player.orientation.cos();
+                    let y_delta = PLAYER_SPEED * data.player.orientation.sin();
+                    message_queue.push_back(Message::Backwards(nalgebra_glm::Vec2::new(
+                        -x_delta, -y_delta,
+                    )));
+                }
+                (Key::A, Some(Message::RotateCounterClockwise(_))) => {}
+                (Key::A, _) => {
+                    message_queue
+                        .push_back(Message::RotateCounterClockwise(-PLAYER_ROTATION_SPEED));
+                }
+                (Key::D, Some(Message::RotateClockwise(_))) => {}
+                (Key::D, _) => {
+                    message_queue.push_back(Message::RotateClockwise(PLAYER_ROTATION_SPEED));
+                }
+                _ => {}
+            });
 
-        frame_count += 1;
+        while let Some(msg) = message_queue.pop_front() {
+            data = update(data, msg);
+        }
+        render(&mut framebuffer, &data);
     }
 }
 
@@ -132,8 +176,36 @@ fn extract_player_starting_position(cells: &[Vec<char>]) -> nalgebra_glm::Vec2 {
     nalgebra_glm::Vec2::zeros()
 }
 
-fn update(data: Model) -> Model {
-    data
+fn update(data: Model, msg: Message) -> Model {
+    let Model {
+        board,
+        framebuffer_dimensions,
+        player,
+    } = data;
+
+    match msg {
+        Message::Advance(delta) | Message::Backwards(delta) => {
+            let position = player.position + delta;
+            let player = Player { position, ..player };
+            Model {
+                board,
+                framebuffer_dimensions,
+                player,
+            }
+        }
+        Message::RotateCounterClockwise(delta) | Message::RotateClockwise(delta) => {
+            let orientation = player.orientation + delta;
+            let player = Player {
+                orientation,
+                ..player
+            };
+            Model {
+                board,
+                framebuffer_dimensions,
+                player,
+            }
+        }
+    }
 }
 
 fn from_char_to_color(c: &char) -> Color {
@@ -180,10 +252,36 @@ fn render(framebuffer: &mut Framebuffer, data: &Model) {
             }
         });
 
-    framebuffer.set_current_color(0x000000);
-    let _ = framebuffer.paint_point(vec2_to_vec3(&data.player.position));
-
     cast_ray_2d(framebuffer, &data.board, &data.player);
+
+    framebuffer.set_current_color(0x0000ff);
+    let _ = framebuffer.paint_point(vec2_to_vec3(&data.player.position));
+}
+
+fn ray_2d(maze: &Board, player: &Player) -> nalgebra_glm::Vec2 {
+    let mut d = 0.0;
+
+    loop {
+        let cos = d * player.orientation.cos();
+        let sin = d * player.orientation.sin();
+
+        let x = player.position.x + cos;
+        let y = player.position.y + sin;
+        let position = nalgebra_glm::Vec2::new(x, y);
+
+        // println!("Checking cords at: {}, {}", x, y);
+
+        let i = (x / maze.cell_dimensions.0) as usize;
+        let j = (y / maze.cell_dimensions.1) as usize;
+        let cell = maze.cells[j][i];
+
+        // println!("Checking cell [{}] at: {}, {}", cell, x, y);
+        if cell == '+' || cell == '-' || cell == '|' {
+            return position;
+        }
+
+        d += 1.0;
+    }
 }
 
 fn cast_ray_2d(framebuffer: &mut Framebuffer, maze: &Board, player: &Player) {
@@ -196,20 +294,20 @@ fn cast_ray_2d(framebuffer: &mut Framebuffer, maze: &Board, player: &Player) {
 
         let x = player.position.x + cos;
         let y = player.position.y + sin;
+        let position = nalgebra_glm::Vec2::new(x, y);
+        let _ = framebuffer.paint_point(vec2_to_vec3(&position));
 
         // println!("Checking cords at: {}, {}", x, y);
 
         let i = (x / maze.cell_dimensions.0) as usize;
         let j = (y / maze.cell_dimensions.1) as usize;
-
-        let _ = framebuffer.paint_point(nalgebra_glm::Vec3::new(x, y, 0.0));
-
         let cell = maze.cells[j][i];
+
         // println!("Checking cell [{}] at: {}, {}", cell, x, y);
         if cell == '+' || cell == '-' || cell == '|' {
             return;
         }
 
-        d += 3.0;
+        d += 1.0;
     }
 }
